8c1d8cde89ca9a230b100e7e691b95c403d8cb4a
Jason Calem <jason.calem@gmail.com>
2020-08-06 12:19:01 -0400

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,6 @@ declare module 'react-native-responsive-screen' {
 
   export function widthPercentageToDP(widthPercent: string | number): number;
   export function heightPercentageToDP(heightPercent: string | number): number;
-  export function listenOrientationChange(screenClassComponent: Component<any, any>): void;
+  export function listenOrientationChange(orientationHookDispatch: Dispatch<any>): void;
   export function removeOrientationListener(): void;
 }
