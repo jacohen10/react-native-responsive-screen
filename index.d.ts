9a2ed083e3df5eeff0511415c5b6d2fa7b16ec4b
Jason Calem <jason.calem@gmail.com>
2020-08-06 12:25:04 -0400

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,4 @@
 declare module 'react-native-responsive-screen' {
-  import { Component } from 'react';
 
   export function widthPercentageToDP(widthPercent: string | number): number;
   export function heightPercentageToDP(heightPercent: string | number): number;
